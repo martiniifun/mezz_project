f7b2acf656ba6bfa2283b9135192eab803ebbb70
Martinii <mjshinetm@gmail.com>
2019-03-11 23:28:03 +0000

    initial commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/smj0226/mezzanine/smj0226.pythonanywhere.com/mezz_project/myvenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
